59bb653e7bb98768fa2c6af35af4d75f141c3d6a
shallins <atutishallins@gmail.com>
2025-04-08 14:51:51 +0000

    second commit

--- assnreact/src/App.jsx
+++ assnreact/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import './App.css'
 function App() {
   const [item, setitem] = useState([]);
   const [input, setInput] = useState('');
@@ -18,38 +18,33 @@ function App() {
   };
 
   return (
-    <div class="container">
+    <div className="container">
       <h1>Dynamic List Manager</h1>
 
-      <div class="input">
+      <div>
         <input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="Enter an item"
-          class  {
-            constructor(parameters) {
-              
-            }
-          }="item"
+          className="item"
         />
         <button
           onClick={AddItem}
         >
-          Add
+          Add Item
         </button>
+        <div/>
+        <div className="list">
     <ul>
         {item.map((item, index) => (
-          <li key={index} class="listitem">
+          <li key={index}>
             <span>{item}</span>
-            <button
-              onClick={() => DeleteItem(index)}
-            >
-              Delete
-            </button>
           </li>
         ))}
       </ul>
+      
+      </div>
       </div>
     </div>
   );
